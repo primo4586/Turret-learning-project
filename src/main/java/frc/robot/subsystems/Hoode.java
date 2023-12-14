9f9a4e62e9baf9f506e2ecf74ab1353701a40a3e
Shaked Naim <shakednaim2006@gmail.com>
2023-12-15 00:02:12 +0200

    started the hoode
    created the basic variables

---
+++ src/main/java/frc/robot/subsystems/Hoode.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Hoode extends SubsystemBase {
+  /** Creates a new Hoode. */
+
+  //created the motor and the degrees the hoode needs to be 
+  private TalonFX m_shooter;
+  private double hoodeDeg;
+
+  //the instance
+  public static Hoode instance;
+
+  public Hoode(double deg) {
+     this.m_shooter = new TalonFX(Constants.HoodeConstants.HoodePort);
+     this.hoodeDeg = deg;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
