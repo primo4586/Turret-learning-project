ffb07355185c1a68fc1a30e3eabccc4e8f608f87
DrorMargalit <dropipopi@gmail,com>
2023-12-15 18:50:01 +0200

    MotionMagicToMotors
    set the motion magic values in the subsystem

--- src/main/java/frc/robot/subsystems/TurrentShot.java
+++ src/main/java/frc/robot/subsystems/TurrentShot.java
@@ -4,27 +4,55 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import frc.robot.Constants;
 import frc.robot.Constants.TurrentShotConstants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurrentShot extends SubsystemBase {
 
-  //Creating the motors
+  // Creating the motors
   private TalonFX shotMotor;
   private TalonFX enterBallMotor;
 
+  // Position voltage
+  private final PositionVoltage m_voltagePosition = new PositionVoltage(0);
+  // Motion magic
+  private final MotionMagicVoltage motionMagic = new MotionMagicVoltage(0);
+
   /** Creates a new TurrentShot. */
   private TurrentShot() {
-    this.shotMotor = new TalonFX(Constants.TurrentShotConstants.KshotMotorID);
-    this.enterBallMotor = new TalonFX(Constants.TurrentShotConstants.KenterBallMotorID);
 
+    // giving values to the motors
+    this.shotMotor = new TalonFX(TurrentShotConstants.KshotMotorID);
+    this.enterBallMotor = new TalonFX(TurrentShotConstants.KenterBallMotorID);
+
+    //declaring Configs
     TalonFXConfiguration configs = new TalonFXConfiguration();
+    MotionMagicConfigs mm = new MotionMagicConfigs();
 
+    //giving motion magic values
+    mm.MotionMagicCruiseVelocity = 5;
+    mm.MotionMagicAcceleration = 10;
+    mm.MotionMagicJerk = 50;
+    configs.MotionMagic = mm;
 
+    //giving PID values
+    configs.Slot0.kP = 24;
+    configs.Slot0.kD = 0.1;
+    configs.Slot0.kV = 0.12;
+    configs.Slot0.kS = 0.25;
+
+    //max voltage for shotMotor
+    configs.Voltage.PeakForwardVoltage = 11.5;
+    configs.Voltage.PeakReverseVoltage = -11.5;
+
+    configs.Feedback.SensorToMechanismRatio = 50;
 
   }
 
