7a66ba78766e7715a52317c6dea61a5875ba0b27
shay <shaykalisher7@gmail.com>
2023-12-16 00:59:27 +0200

    creating camera, photonEstimator, setMultiTagFallbackStrategy
    creating camera, photonEstimator, setMultiTagFallbackStrategy

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,12 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +21,14 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+  public static class VisionConstants{
+    public static final String FRONT_CAMERA_NAME = "FRONT CAMERA NAME";
+    public static final AprilTagFieldLayout kTagLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0, 0, 0));
+
+
+
+
   }
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -3,6 +3,32 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+ 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.units.Distance;
+import frc.robot.Constants;
 /** Add your docs here. */
-public class Vision {}
+public class Vision {
+    private AprilTagFieldLayout kTagLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField();
+    private final PhotonCamera Camera;
+    private final PhotonPoseEstimator photonEstimator;
+
+    
+    public Vision(){
+        Camera = new PhotonCamera(Constants.VisionConstants.FRONT_CAMERA_NAME);
+        photonEstimator = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, Constants.VisionConstants.kRobotToCam);
+        //photonEstimator = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Camera, Constants.VisionConstants.kRobotToCam);
+        photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+
+    }
+
+    public enum PoseStrategy{
+        MULTI_TAG_PNP_ON_COPROCESSOR,
+        LOWEST_AMBIGUITY
+    }
+}
