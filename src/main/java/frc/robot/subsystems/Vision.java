7d72cfc063413017bb6a6934044ab1898d71ee04
shay <shaykalisher7@gmail.com>
2023-12-15 17:29:00 +0200

    deleting the subsystem and create class Vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,14 +4,5 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Vision extends SubsystemBase {
-  /** Creates a new Vision. */
-  public Vision() {}
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-}
+/** Add your docs here. */
+public class Vision {}
