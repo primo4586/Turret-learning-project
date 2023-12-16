b2887444832c8f13ae29253dfb5b45d764bacab9
DrorMargalit <dropipopi@gmail,com>
2023-12-16 20:30:45 +0200

    ChangesAtTheSubsystem

--- src/main/java/frc/robot/commands/ShooterMotorCommand.java
+++ src/main/java/frc/robot/commands/ShooterMotorCommand.java
@@ -4,16 +4,22 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubsystem;
+import static frc.robot.Constants.ShooterConstants.*;
 
 public class ShooterMotorCommand extends Command {
   private final ShooterSubsystem shooter = ShooterSubsystem.getInstance();
+  double shooterSpeed;
+  Timer timer;
 
   /** Creates a new ShooterMotorCommand. */
   public ShooterMotorCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.shooterSpeed = shooterSpeed;
     this.addRequirements(shooter);
+    this.timer = new Timer();
   }
 
   // Called when the command is initially scheduled.
@@ -23,16 +29,18 @@ public class ShooterMotorCommand extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    this.shooter.setShooterSpeed(shooterSpeed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    this.shooter.setShooterSpeed(0);
   }
-
+  
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
-  }
+    }
 }
