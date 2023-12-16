aba027e2046afca7e09cb6d2a230985db4d68597
DrorMargalit <dropipopi@gmail,com>
2023-12-16 15:45:55 +0200

    AddedToTheSubsystemAndCreatedShooterCommand

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,17 +1,16 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import frc.robot.Constants.ShooterConstants;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
@@ -21,49 +20,62 @@ public class ShooterSubsystem extends SubsystemBase {
   private TalonSRX m_feederMotor;
 
   // Motion magic
-  private final MotionMagicVoltage motionMagic = new MotionMagicVoltage(0);
+  private final MotionMagicVelocityVoltage motionMagic = new MotionMagicVelocityVoltage(0);
 
   /** Creates a new TurrentShot. */
-  private ShooterSubsystem() {    
+  private ShooterSubsystem() {
 
     // giving values to the motors
     this.m_shooterMotor = new TalonFX(ShooterConstants.KShooterMotorID);
     this.m_feederMotor = new TalonSRX(ShooterConstants.KFeederMotorID);
 
-    //declaring Configs
+    // declaring Configs
     TalonFXConfiguration configs = new TalonFXConfiguration();
     MotionMagicConfigs mm = new MotionMagicConfigs();
 
-    //giving motion magic values
+    // giving motion magic values
     mm.MotionMagicCruiseVelocity = ShooterConstants.MotionMagicCruiseVelocity;
     mm.MotionMagicAcceleration = ShooterConstants.MotionMagicAcceleration;
     mm.MotionMagicJerk = ShooterConstants.MotionMagicJerk;
     configs.MotionMagic = mm;
 
-    //giving PID values
-    configs.Slot0.kP = 24;
-    configs.Slot0.kD = 0.1;
-    configs.Slot0.kV = 0.12;
-    configs.Slot0.kS = 0.25;
+    // giving PID values
+    configs.Slot0.kP = ShooterConstants.kP;
+    configs.Slot0.kD = ShooterConstants.kD;
+    configs.Slot0.kV = ShooterConstants.kV;
+    configs.Slot0.kS = ShooterConstants.kS;
 
-    //max voltage for shotMotor
+    // max voltage for shotMotor
     configs.Voltage.PeakForwardVoltage = ShooterConstants.PeakForwardVoltage;
     configs.Voltage.PeakReverseVoltage = ShooterConstants.PeakReverseVoltage;
 
     configs.Feedback.SensorToMechanismRatio = ShooterConstants.SensorToMechanismRatio;
-    
+
     StatusCode status = StatusCode.StatusCodeNotInitialized;
-    for (int i = 0; i<5; ++i){
+    for (int i = 0; i < 5; ++i) {
       status = m_shooterMotor.getConfigurator().apply(configs);
-      if(status.isOK())
+      if (status.isOK())
         break;
     }
-    if (!status.isOK()){
+    if (!status.isOK()) {
       System.out.println("Could not apply configs, error code: " + status.toString());
     }
 
     configs.Feedback.SensorToMechanismRatio = ShooterConstants.SensorToMechanismRatio;
 
+  }
+
+  private static ShooterSubsystem instance;
+
+  public static ShooterSubsystem getInstance(){
+    if (instance==null){
+      instance = new ShooterSubsystem();
+    }
+    return instance;
+  }
+
+  public void setShooterSpeed(double shooterSpeed){
+    this.m_shooterMotor.setControl(motionMagic.withVelocity(shooterSpeed));
   }
 
   @Override
